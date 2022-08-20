aaba0e08b65fa8f3e38798b29ed359a5deb75bcd
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2022-08-20 10:17:58 -0400

    feat: mantine styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,21 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/dist/shared/lib/utils";
 import { trpc } from "../utils/trpc";
+import { MantineProvider } from "@mantine/core";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
