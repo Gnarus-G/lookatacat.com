6a1f741b7c2c4823d042fde13bff22eaeaa155d4
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-05-15 21:56:40 -0400

    chore: update next configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
  * @template {import('next').NextConfig} T
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
@@ -14,7 +11,11 @@ export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [new URL(process.env.NEXT_PUBLIC_WORKER_ENDPOINT).hostname],
+    remotePatterns: [
+      {
+        hostname: new URL(process.env.NEXT_PUBLIC_WORKER_ENDPOINT).hostname,
+      },
+    ],
   },
   async redirects() {
     return [
